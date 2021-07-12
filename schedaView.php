37b72d95c9c1ef44b8267e492255356179398bde
Giuseppe Naponiello <beppenapo@arc-team.com>
2021-07-12 14:21:51 +0200

    scheda view inizializzazione

--- schedaView.php
+++ schedaView.php
@@ -1,16 +1,36 @@
+<?php
+require("api/php/scheda.php");
+?>
 <!DOCTYPE html>
 <html lang="it" dir="ltr">
   <head>
     <?php require('assets/meta.html'); ?>
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/scheda.css">
   </head>
   <body>
     <?php require('assets/headerMenu.php'); ?>
     <?php require('assets/mainMenu.php'); ?>
+    <div id="loadingDiv" class="flexDiv invisible"><i class='fas fa-circle-notch fa-spin fa-5x'></i></div>
     <main>
+      <div class="bg-dark px-3">
+        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+          <div class="btn-group" role="group">
+            <button id="btnGroupDrop1" type="button" class="btn btn-dark dropdown-toggle py-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown
+            </button>
+            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+              <a class="dropdown-item" href="#">Dropdown link</a>
+              <a class="dropdown-item" href="#">Dropdown link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
     </main>
+    <?php require('assets/toast.html'); ?>
     <?php require('assets/footer.html'); ?>
     <?php require('assets/lib.html'); ?>
     <script src="js/function.js" charset="utf-8"></script>
+    <script src="js/scheda-ra.js" charset="utf-8"></script>
   </body>
 </html>
