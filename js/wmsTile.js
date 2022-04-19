2a29f940e78f0d25b7a2f28a7f23e16b4ed1138f
Giuseppe Naponiello <beppenapo@arc-team.com>
2022-04-19 12:38:54 +0200

    variabili geometriche

---
+++ js/wmsTile.js
@@ -0,0 +1,11 @@
+const osmTile = 'https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png';
+const osmAttrib='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors';
+const gStreetTile = 'http://{s}.google.com/vt/lyrs=m&x={x}&y={y}&z={z}';
+const gSatTile = 'http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}';
+const gHybridTile = 'http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}';
+const gTerrainTile = 'http://{s}.google.com/vt/lyrs=p&x={x}&y={y}&z={z}';
+const gSubDomains = ['mt0','mt1','mt2','mt3']
+const bingKey = "Arsp1cEoX9gu-KKFYZWbJgdPEa8JkRIUkxcPr8HBVSReztJ6b0MOz3FEgmNRd4nM";
+const thunderFKey = "f1151206891e4ca7b1f6eda1e0852b2e";
+const thunderFTile = 'https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey='+thunderFKey;
+const thunderFAttrib = 'Maps &copy; <a href="https://www.thunderforest.com">Thunderforest</a>, Data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>';
