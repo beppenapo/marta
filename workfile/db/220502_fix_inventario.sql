fb430caa26c9e949b6c108b597c8fe0bbfe299f4
Giuseppe Naponiello <beppenapo@arc-team.com>
2022-05-06 12:10:47 +0200

    fix sql

--- workfile/db/220502_fix_inventario.sql
+++ workfile/db/220502_fix_inventario.sql
@@ -3,7 +3,8 @@ alter table inventario add column scheda integer references scheda(id) on delete
 update inventario set scheda = s.scheda from inventario_scheda s where s.inventario = inventario.id;
 delete from inventario where scheda is null;
 alter table inventario alter column scheda set not null;
-drop table inventario_scheda;
+drop table inventario_scheda cascade;
 alter table inventario drop constraint inventario_inventario_prefisso_suffisso_key ;
 alter table inventario add constraint inventario_unique UNIQUE (inventario, prefisso, suffisso,scheda);
+alter table inventario owner to marta;
 commit;
