2213dd0756bb0b97639dd147bd0ac7a847e3cd8a
Giuseppe Naponiello <beppenapo@arc-team.com>
2022-10-13 10:07:38 +0200

    db munsell

--- workfile/csv/munsell_completo.sql
+++ workfile/csv/munsell_completo.sql
@@ -9,8 +9,8 @@ create table liste.munsell(
 );
 copy liste.munsell(gruppo, code, color) from '/var/www/marta/workfile/csv/munsell_completo.csv' delimiter ',' csv header;
 
-alter table munsell add column m_id integer;
-alter table munsell add constraint munsell_fki foreign key (m_id) references liste.munsell(id) on delete cascade;
+--alter table munsell add column m_id integer;
+--alter table munsell add constraint munsell_fki foreign key (m_id) references liste.munsell(id) on delete cascade;
 update munsell set m_id = m.id from liste.munsell m where munsell.munsell = concat(m.gruppo,' ',m.code);
 alter table munsell drop column munsell;
 alter table munsell rename column m_id to munsell;
