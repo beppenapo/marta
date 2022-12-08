7a7d604b886b73eb1c56beee414fa843c207673d
Giuseppe Naponiello <beppenapo@arc-team.com>
2022-12-08 16:28:54 +0100

    importazione file sql

--- workfile/db/221207_importazione_file.sql
+++ workfile/db/221207_importazione_file.sql
@@ -2,5 +2,5 @@ begin;
 drop table if exists import;
 create table import(file character varying primary key, nctn integer);
 alter table import owner to marta;
-copy import from '/var/www/marta/workfile/csv/file.csv' delimiter ',' csv header;
+copy import from '/var/www/html/marta/workfile/csv/file.csv' delimiter ',' csv header;
 commit;
