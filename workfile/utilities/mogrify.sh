8d3f6593515d8102d02fd2618581929f1a148ef3
Giuseppe Naponiello <beppenapo@arc-team.com>
2022-12-19 15:11:57 +0100

    mogrify

--- workfile/utilities/mogrify.sh
+++ workfile/utilities/mogrify.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mogrify -verbose -path "file/foto_small/" -shave 30x30 -thumbnail 150x150^ -gravity center -extent 150x150  foto/*.jpg
-mogrify -verbose -path "file/foto_medium/" -shave 30x30 -thumbnail 250x250^ -gravity center -extent 250x250  foto/*.jpg
+mogrify -verbose -path "file/foto_small/" -shave 30x30 -thumbnail 150x150^ -gravity center -extent 150x150 "file/foto/*.jpg"
+mogrify -verbose -path "file/foto_medium/" -shave 30x30 -thumbnail 250x250^ -gravity center -extent 250x250 "file/foto/*.jpg"
