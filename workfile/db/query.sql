2255f8518b53dec0398167904b12eaf6a218c97c
Giuseppe Naponiello <beppenapo@arc-team.com>
2023-07-12 17:45:47 +0200

    stampa e altre cose

--- workfile/db/query.sql
+++ workfile/db/query.sql
@@ -1,27 +1,3 @@
 begin;
-insert into biblio_fake(scheda, riferimento) values
-(43238, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43183, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43184, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43193, 'R. Belli Pasqua, Taranto la scultura in marmo e in pietra (Catalogo del Museo Nazionale Archeologico di Taranto IV.1), 1995'),
-(43230, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43235, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43234, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43159, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43162, 'E. Lippolis, S. Garraffo, M. Nafissi, Culti greci in Occidente, 1.Taranto, Taranto 1995.'),
-(43182, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43228, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43194, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43152, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43121, 'E. Lippolis, S. Garraffo, M. Nafissi, Culti greci in Occidente, 1.Taranto, Taranto 1995.'),
-(43185, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43233, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43239, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43240, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203'),
-(43118, 'G. Mastrocinque, Tranto. Il paesaggio urbano di età romana tra persistenza e innovazione, Napoli 2010.'),
-(43119, 'E. Lippolis, S. Garraffo, M. Nafissi, Culti greci in Occidente, 1.Taranto, Taranto 1995.'),
-(43117, 'E. Lippolis, S. Garraffo, M. Nafissi, Culti greci in Occidente, 1.Taranto, Taranto 1995.'),
-(43122, 'E. Lippolis, S. Garraffo, M. Nafissi, Culti greci in Occidente, 1.Taranto, Taranto 1995.'),
-(43231, 'L. Gasperini, "Il municipio tarentino. Ricerche epigrafiche", in Terza Miscellanea greca e romana , Roma 1971, pp. 143-203');
 
 commit;
