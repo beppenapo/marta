3d329a927296ebcb91a44987a508aecef6a39021
Giuseppe Naponiello <beppenapo@arc-team.com>
2024-07-11 10:59:29 +0200

    query.sql

--- workfile/db/query.sql
+++ workfile/db/query.sql
@@ -1,3 +1,12 @@
-begin;
-
-commit;
+select distinct 
+  -- gallery.*, 
+  file.* 
+from gallery, liste.vetrine contenitore, file 
+where 
+  file.tipo = 3
+  and (substring(file.file from 16 for 3) = '_A_' or substring(file.file from 16 for 4) = '_02_')
+  and contenitore.sala = gallery.sala_id 
+  and contenitore.vetrina = gallery.contenitore
+  and gallery.piano = 1
+  and file.scheda = gallery.id 
+order by 1 asc;
