c20413407151538792538707950bd90448993244
Giuseppe Naponiello <beppenapo@arc-team.com>
2024-07-12 18:12:35 +0200

    pianta primo piano svg

--- workfile/db/query.sql
+++ workfile/db/query.sql
@@ -1,12 +1,9 @@
-select distinct 
-  -- gallery.*, 
-  file.* 
-from gallery, liste.vetrine contenitore, file 
-where 
-  file.tipo = 3
-  and (substring(file.file from 16 for 3) = '_A_' or substring(file.file from 16 for 4) = '_02_')
-  and contenitore.sala = gallery.sala_id 
-  and contenitore.vetrina = gallery.contenitore
-  and gallery.piano = 1
-  and file.scheda = gallery.id 
-order by 1 asc;
+begin;
+update file set scheda = 13974 where id = 86986;
+update file set scheda = 13974 where id = 88666;
+update file set scheda = 13974 where id = 82696;
+update file set scheda = 13972 where id = 91870;
+update file set scheda = 13972 where id = 85428;
+update file set scheda = 13972 where id = 84853;
+update file set scheda = 13972 where id = 82638;
+commit;
